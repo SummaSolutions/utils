c6e1bece13317a86eb252faaae92334d806bbec7
Matias.Hidalgo <mhidalgo@summasolutions.net>
2015-05-26 18:00:07 -0300

    re #AndreaniModule Code Reviews

--- Magento/Andreani/app/code/community/Summa/Andreani/sql/mysql4-upgrade-QUOTE_ORDER_ADDRESS_ATTRIBUTES.php
+++ Magento/Andreani/app/code/community/Summa/Andreani/sql/mysql4-upgrade-QUOTE_ORDER_ADDRESS_ATTRIBUTES.php
@@ -12,19 +12,93 @@ $installer = $this;
 $salesSetup = Mage::getResourceModel('sales/setup', 'core_setup');
 
 $installer->startSetup();
-$entities = array('quote_address', 'order_address');
+$entities = array('customer_address', 'quote_address', 'order_address');
 
 $fields  = array(
-    'dni',
-    'number',
-    'floor',
-    'apartment'
+    'dni'       => array(
+        'type'                          => 'varchar',
+        'label'                         => 'DNI',
+        'input'                         => 'text',
+        'required'                      => 0,
+        'global'                        => 1,
+        'visible'                       => 1,
+        'user_defined'                  => 1,
+        'searchable'                    => 0,
+        'filterable'                    => 0,
+        'comparable'                    => 0,
+        'visible_on_front'              => 1,
+        'visible_in_advanced_search'    => 0,
+        'unique'                        => 0,
+        'is_configurable'               => 0,
+        'position'                      => 200
+    ),
+    'number'    => array(
+        'type'                          => 'varchar',
+        'label'                         => 'Number',
+        'input'                         => 'text',
+        'required'                      => 0,
+        'global'                        => 1,
+        'visible'                       => 1,
+        'user_defined'                  => 1,
+        'searchable'                    => 0,
+        'filterable'                    => 0,
+        'comparable'                    => 0,
+        'visible_on_front'              => 1,
+        'visible_in_advanced_search'    => 0,
+        'unique'                        => 0,
+        'is_configurable'               => 0,
+        'position'                      => 201
+    ),
+    'floor'     => array(
+        'type'                          => 'varchar',
+        'label'                         => 'Floor',
+        'input'                         => 'text',
+        'required'                      => 0,
+        'global'                        => 1,
+        'visible'                       => 1,
+        'user_defined'                  => 1,
+        'searchable'                    => 0,
+        'filterable'                    => 0,
+        'comparable'                    => 0,
+        'visible_on_front'              => 1,
+        'visible_in_advanced_search'    => 0,
+        'unique'                        => 0,
+        'is_configurable'               => 0,
+        'position'                      => 202
+    ),
+    'apartment' => array(
+        'type'                          => 'varchar',
+        'label'                         => 'Apartment',
+        'input'                         => 'text',
+        'required'                      => 0,
+        'global'                        => 1,
+        'visible'                       => 1,
+        'user_defined'                  => 1,
+        'searchable'                    => 0,
+        'filterable'                    => 0,
+        'comparable'                    => 0,
+        'visible_on_front'              => 1,
+        'visible_in_advanced_search'    => 0,
+        'unique'                        => 0,
+        'is_configurable'               => 0,
+        'position'                      => 203
+    )
 );
-$options = array('type' => Varien_Db_Ddl_Table::TYPE_VARCHAR);
 
 foreach ($entities as $entity) {
-    foreach ($fields as $field){
-        $salesSetup->addAttribute($entity, $field, $options );
+    foreach ($fields as $code => $fieldOptions){
+        $salesSetup->addAttribute($entity, $code, $fieldOptions );
     }
+}
+
+$fieldsets = array('customer_register_address',
+    'customer_address_edit',
+    'adminhtml_customer_address');
+
+foreach($fields as $code => $fieldOptions){
+    Mage::getModel('eav/config')
+        ->getAttribute('customer_address', $code)
+        ->setData('used_in_forms', $fieldsets)
+        ->save();
 }
 $installer->endSetup();
